372c056038834fd1e0286c8aec483eb9b9fcfb9b
Brenden Matthews <brenden@diddyinc.com>
2018-02-17 16:55:36 -0500

    Fix copyright year.

--- src/main/kotlin/org/ompldr/server/PeriodicService.kt
+++ src/main/kotlin/org/ompldr/server/PeriodicService.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright 2008 Brenden Matthews <brenden@diddyinc.com>
+ * Copyright 2018 Brenden Matthews <brenden@diddyinc.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
