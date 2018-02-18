19a1ef37ed61d68e38d8436b47cbfc1ec808c849
Brenden Matthews <brenden@diddyinc.com>
2018-02-18 13:38:12 -0500

    Test new build.

--- start.sh
+++ start.sh
@@ -5,13 +5,12 @@ envsubst < /etc/ompldr-config/application.conf.in > application.conf
 
 cat application.conf
 
-exec java -server \
-  -Xms2g \
-  -Xmx2g \
-  -XX:+UseConcMarkSweepGC \
-  -XX:+UseStringDeduplication \
-  -XX:+UseCompressedOops \
-  -Dconfig.file=application.conf \
-  $JVM_OPTS \
-  -jar ompldr-server.jar \
+OMPLDR_SERVER_OPTS="-Dconfig.file=application.conf"
+JAVA_OPTS="-XX:+UseCompressedOops $JAVA_OPTS"
+JAVA_OPTS="-XX:+UseStringDeduplication $JAVA_OPTS"
+JAVA_OPTS="-XX:+UseConcMarkSweepGC $JAVA_OPTS"
+JAVA_OPTS="-Xmx2g $JAVA_OPTS"
+JAVA_OPTS="-Xms2g $JAVA_OPTS"
+JAVA_OPTS="-server $JAVA_OPTS"
+exec java $JAVA_OPTS $OMPLDR_SERVER_OPTS -jar ompldr-server.jar \
   "$@"
