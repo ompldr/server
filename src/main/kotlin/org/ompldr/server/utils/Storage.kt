5da108bdd4357d1ab8cfd8e18998d5ec27890ba1
Brenden Matthews <brenden@diddyinc.com>
2018-02-26 07:49:55 -0500

    Bump base fee to $0.25.

--- src/main/kotlin/org/ompldr/server/utils/Storage.kt
+++ src/main/kotlin/org/ompldr/server/utils/Storage.kt
@@ -181,7 +181,7 @@ private val pricePerByteofTransfer =
 
 fun calculatePriceInUSD(quoteRequest: QuoteRequest): BigDecimal {
   val totalBytesToServe = BigDecimal(quoteRequest.length * quoteRequest.downloadCount)
-  return BigDecimal(0.1) // minimum price, $0.10
+  return BigDecimal(0.25) // minimum price, $0.25
       .plus(pricePerByteOfStoragePerSecond
           .multiply(
               totalBytesToServe.multiply(BigDecimal(quoteRequest.expiresAfterSeconds))))
